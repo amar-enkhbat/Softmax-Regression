7693f5c6a1161863fba49f562cd36c1c46b81a7b
Amar Enkhbat <38126496+amar-enkhbat@users.noreply.github.com>
2018-05-23 22:59:53 +0900

    Added non-linear version

--- nonlinear_softmax.py
+++ nonlinear_softmax.py
@@ -1,12 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 23 20:19:01 2018
+
+@author: Amar Enkhbat
+"""
+
 import matplotlib.pyplot as plt 
 import numpy as np 
 
 np.set_printoptions(suppress=True)
 
 from sklearn import datasets
-iris = datasets.load_iris()
-X = iris.data[:, [2, 3]]
-y = iris.target
+wine = datasets.load_wine()
+X = wine.data[:, [2, 8]]
+y = wine.target
 
 class_labels = int(y.max() + 1)
 # Data plot
@@ -14,15 +21,15 @@ class_labels = int(y.max() + 1)
 #    plt.scatter(X[y == i, 0], X[y == i, 1], label = "Class" + str(i))
 #plt.show()
 # =============================================================================
-feature_1 = np.array([2, 5, 8, 4], dtype = float)
-feature_2 = np.array([5, 1, 6, 9], dtype = float)
- 
- # sample
-X = np.column_stack((feature_1, feature_2))
- 
- # label
-y = np.arange(len(feature_1))
-class_labels = int(y.max() + 1)
+#feature_1 = np.array([2, 5, 8, 4], dtype = float)
+#feature_2 = np.array([5, 1, 6, 9], dtype = float)
+# 
+# # sample
+#X = np.column_stack((feature_1, feature_2))
+# 
+# # label
+#y = np.arange(len(feature_1))
+#class_labels = int(y.max() + 1)
 # =============================================================================
 # Data standardization
 from sklearn.preprocessing import StandardScaler
@@ -36,10 +43,12 @@ train_test_split = np.c_[X_train, y]
 
 # Initiate weights
 rgen = np.random.RandomState(2)
-weight = rgen.normal(scale = 0.01, size = (X.shape[1] + 1, len(np.unique(y))))
+weight = rgen.normal(scale = 0.01, size = (X.shape[1] + 3, len(np.unique(y))))
 rgen.shuffle(train_test_split)
 X_train = train_test_split[:, :X.shape[1]]
 y = train_test_split[:, X.shape[1]]
+
+X_train = np.c_[X_train, X_train**2]
 
 def one_hot_encoder(y):
     a = np.zeros((len(y), class_labels))
@@ -76,10 +85,10 @@ for epochs in range(epoch):
     weight[1:] -= eta * (grad + cost_lambda * weight[1:] / X_train.shape[1])
     weight[0] -= eta*np.sum(diff, axis = 0)
 
-    if len(cost_series) > 2:
-        if cost_series[-2] - cost_series[-1] < 0.0001:
-            break
-
+#    if len(cost_series) > 2:
+#        if cost_series[-1] - cost_series[-2] < 0.00001:
+#            break
+    
 plt.plot(range(len(cost_series)), cost_series)
 plt.show()
 
@@ -117,7 +126,7 @@ print(accuracy)
 # Data plot
 
 from matplotlib.colors import ListedColormap
-def plot_decision_regions(X, y, weight, resolution = 0.02):
+def plot_decision_regions(X, y, weight, resolution = 0.01):
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
@@ -125,8 +134,7 @@ def plot_decision_regions(X, y, weight, resolution = 0.02):
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-
-    Z = softmax(np.array([xx1.ravel(), xx2.ravel()]).T, weight)
+    Z = softmax(np.array([xx1.ravel(), xx2.ravel(), xx1.ravel()**2, xx2.ravel()**2]).T, weight)
     Z = Z.argmax(axis = 1)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha = 0.3, cmap = cmap)
@@ -136,27 +144,5 @@ def plot_decision_regions(X, y, weight, resolution = 0.02):
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], alpha = 0.8, c = colors[idx], marker = markers[idx], label = cl, edgecolor = 'black')
 
-def plot_decision_regions1(X, y, classifier, resolution = 0.02):
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha = 0.3, cmap = cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], alpha = 0.8, c = colors[idx], marker = markers[idx], label = cl, edgecolor = 'black')
-
-
 plot_decision_regions(X_train, y, weight)
-plt.show()
-
-plot_decision_regions1(X_train, y, classifier = classifier)
 plt.show()
