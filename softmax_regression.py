e205e2e8cd7c9c3e40387ca21b06203038c12206
Amar Enkhbat <38126496+amar-enkhbat@users.noreply.github.com>
2018-06-09 15:01:31 +0900

    Successfully added functions

--- softmax_regression.py
+++ softmax_regression.py
@@ -1,27 +1,36 @@
 
 # モジュールをインポート
-import matplotlib.pyplot as plt 
 import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib.colors import ListedColormap
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.linear_model import LogisticRegression
 
+# =============================================================================
+# Default settings
+# =============================================================================
+# Supress scientific notations
 np.set_printoptions(suppress=True)
-#　ランダムシード
-rgen = np.random.RandomState(2)
 
-# サンプルロード
+# Define a random seed
+random_seed = 123
+np.random.seed(random_seed)
+
+# =============================================================================
+# Import samples
+# =============================================================================
 iris = datasets.load_iris()
 X = iris.data[:, [0,3]]
 y = iris.target
 
-# クラスラベル数
-class_labels = int(y.max() + 1)
-
-# Train, test, split
-from sklearn.model_selection import train_test_split
-# サンプルをシャフル
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 2)
-X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.25, random_state = 2)
+# =============================================================================
+# Separate samples
+# =============================================================================
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = random_seed)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.25, random_state = random_seed)
 
 # Data standardization
 stdsc = StandardScaler()
@@ -29,11 +38,15 @@ X_train_std = stdsc.fit_transform(X_train)
 X_valid_std = stdsc.transform(X_valid)
 X_test_std = stdsc.transform(X_valid)
 
+# =============================================================================
+# Weight initialization
+# =============================================================================
+weight = np.random.normal(scale = 0.01, size = (X.shape[1] + 1, len(np.unique(y))))
 
-# 重みを初期化
-weight = rgen.normal(scale = 0.1, size = (X.shape[1] + 1, len(np.unique(y))))
-
-# 符号化関数
+# =============================================================================
+# One hot encoder
+# =============================================================================
+class_labels = int(y.max() + 1)
 def one_hot_encoder(y):
     a = np.zeros((len(y), class_labels))
     for idx, i in enumerate(y):
@@ -44,48 +57,92 @@ y_train_coded = one_hot_encoder(y_train)
 y_valid_coded = one_hot_encoder(y_valid)
 y_test_coded= one_hot_encoder(y_test)
 
-
+# =============================================================================
 # Softmax function
-def softmax(X, weight):
-    net_input = X.dot(weight[1:]) + weight[0]
-    prob = np.exp(net_input)
-    prob = prob / np.exp(net_input).sum(axis = 1).reshape(-1, 1)
+# =============================================================================
+def softmax(z):
+    prob = np.exp(z)
+    prob = prob / np.exp(z).sum(axis = 1).reshape(-1, 1)
     return prob
 
+# =============================================================================
+# Net input function
+# =============================================================================
+def net_input(X, weight):
+    return X.dot(weight[1:]) + weight[0]
+# =============================================================================
+# Defining variables
+# =============================================================================
+
 # Number of epochs
-epochs = 100
+epochs = 1000
 
 # Learning rate
 learning_rate = 0.01
-cost_array = []
-cost_lambda = 0.1
-for epoch in range(epochs):
-    z = softmax(X_train_std, weight)
-    y_pred = z.argmax(axis = 1)
-    y_pred_enc = one_hot_encoder(y_pred)
-    regularization = (cost_lambda * (weight[1:]**2).sum()) / (2 * X_train_std.shape[1])
-    cost = -np.sum(np.log(z) * y_pred_enc) + regularization
-#    print(cost)
-    cost_array.append(cost) 
 
-    diff = z - y_train_coded
-    grad = np.dot(X_train_std.T, diff)
-    
-    weight[1:] -= learning_rate * (grad + cost_lambda * weight[1:] / X_train.shape[1])
-    weight[0] -= learning_rate * np.sum(diff, axis = 0)
+# Regularization Lambda
+cost_lambda = 0
 
+# =============================================================================
+# Predict function
+# =============================================================================
+def predict(activation):
+    y_predicted = activation.argmax(axis = 1)
+    return y_predicted
+
+# =============================================================================
+# Full predict function
+# =============================================================================
+def full_predict(X, weight):
+    z = net_input(X, weight)
+    activation = softmax(z)
+    y_predicted = activation.argmax(axis = 1)
+    return y_predicted
     
-    if len(cost_array) > 2:
-        if cost_array[-2] - cost_array[-1] < 0.0001:
-            break
+# =============================================================================
+# Compute cost function
+# =============================================================================
+def compute_cost(X, y):
+    regularization = (cost_lambda * (weight[1:]**2).sum()) / (2 * X.shape[1])
+    z = net_input(X, weight)
+    activation = softmax(z)
+    y_predicted = predict(activation)
+    y_pred_enc = one_hot_encoder(y_predicted)
+    cost = -np.sum(np.log(activation) * y_pred_enc) + regularization
+    return cost
+
+# =============================================================================
+# Training function
+# =============================================================================
+def train(X, y, weight, epochs, learning_rate, cost_lambda):
+    cost_array = []
+    for epoch in range(epochs):
+        z = net_input(X, weight) 
+        activation = softmax(z)
+        cost = compute_cost(X, y)
+        cost_array.append(cost) 
+        diff = activation - y_train_coded
+        grad = np.dot(X.T, diff)
         
+        weight[1:] -= learning_rate * (grad + cost_lambda * weight[1:] / X.shape[1])
+        weight[0] -= learning_rate * np.sum(diff, axis = 0)
+    return weight, cost_array
+
+# =============================================================================
+# Train the classifier
+# =============================================================================
+learned_weights, cost_array = train(X_train_std, y_train, weight, epochs, learning_rate, cost_lambda)
+
+# =============================================================================
+# Plot the cost-epoch graph
+# =============================================================================
 plt.plot(range(len(cost_array)), cost_array)
 plt.show()
 
-z = softmax(X_train_std, weight)
-y_pred = z.argmax(axis = 1)
- 
-from sklearn.metrics import confusion_matrix, accuracy_score
+# =============================================================================
+# Evaluation
+# =============================================================================
+y_pred = full_predict(X_train_std, learned_weights)
 print("")
 print("テストデータでの性能：")
 conf_mat = confusion_matrix(y_train, y_pred)
@@ -95,14 +152,13 @@ accuracy = accuracy_score(y_train, y_pred)
 print("精度:")
 print(accuracy)
 
-
-from sklearn.linear_model import LogisticRegression
-
+# =============================================================================
+# Evaluation of Softmax using LogisticRegression from sklearn
+# =============================================================================
 classifier = LogisticRegression(C = 10)
 classifier.fit(X_train_std, y_train)
 y_pred = classifier.predict(X_train_std)
 
-from sklearn.metrics import confusion_matrix, accuracy_score
 print("")
 print("テストデータでの性能：")
 conf_mat = confusion_matrix(y_train, y_pred)
@@ -115,7 +171,6 @@ print(accuracy)
 
 # Data plot
 
-from matplotlib.colors import ListedColormap
 def plot_decision_regions(X, y, weight, resolution = 0.02):
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -124,9 +179,7 @@ def plot_decision_regions(X, y, weight, resolution = 0.02):
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-
-    Z = softmax(np.array([xx1.ravel(), xx2.ravel()]).T, weight)
-    Z = Z.argmax(axis = 1)
+    Z = full_predict(np.array([xx1.ravel(), xx2.ravel()]).T, weight)
     Z = Z.reshape(xx1.shape)
     plt.contourf(xx1, xx2, Z, alpha = 0.3, cmap = cmap)
     plt.xlim(xx1.min(), xx1.max())
