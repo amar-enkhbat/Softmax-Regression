66bccfb2d701105e00ac831045a749dc8a0afd04
Amar Enkhbat <38126496+amar-enkhbat@users.noreply.github.com>
2018-12-10 14:00:34 +0900

    wine classification

--- softmax_regression.py
+++ softmax_regression.py
@@ -29,35 +29,11 @@ X = wine.data[:, [0, 3]]
 y = wine.target
 number_of_classes = len(np.unique(y))
 
-def load_mnist(path, kind='train'):
-    """Load MNIST data from `path`"""
-    labels_path = os.path.join(path, 
-                               '%s-labels.idx1-ubyte' % kind)
-    images_path = os.path.join(path, 
-                               '%s-images.idx3-ubyte' % kind)
-        
-    with open(labels_path, 'rb') as lbpath:
-        magic, n = struct.unpack('>II', 
-                                 lbpath.read(8))
-        labels = np.fromfile(lbpath, 
-                             dtype=np.uint8)
-
-    with open(images_path, 'rb') as imgpath:
-        magic, num, rows, cols = struct.unpack(">IIII", 
-                                               imgpath.read(16))
-        images = np.fromfile(imgpath, 
-                             dtype=np.uint8).reshape(len(labels), 784)
-        images = ((images / 255.) - .5) * 2
- 
-    return images, labels
-
-X_train, y_train = load_mnist('./mnist/', kind = 't10k')
-X_test, y_test = load_mnist('./mnist/', kind = 't10k')
 # =============================================================================
 # Separate samples
 # =============================================================================
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = random_seed)
-# X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.2, random_state = random_seed)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = random_seed)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.2, random_state = random_seed)
 
 
 
